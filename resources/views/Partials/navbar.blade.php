00384469cf26db16e4dcac2de0aaa0d35342cf26
alberpamungkas <albertpamungkas597@gmail.com>
2026-04-28 00:02:09 +0700

    refactor: improve blade with partial table

---
+++ resources/views/Partials/navbar.blade.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="/">Movie App</a>
+        <div>
+            <a href="/" class="btn btn-light">Home</a>
+            <a href="/movies/data" class="btn btn-light">Data</a>
+        </div>
+    </div>
+</nav>
